7f997520a330253996587e6af6d2c65dfd799bd6
Brandon Haley <brandon.haley94@gmail.com>
2021-04-03 16:11:33 -0700

    Implemented the timer function

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -2,13 +2,24 @@ use x86_64::structures::idt::{
     InterruptDescriptorTable,
     InterruptStackFrame,
 };
-use crate::println;
+use crate::{
+    println,
+    print,
+};
 use lazy_static::lazy_static;
 use pic8259_simple::ChainedPics;
 
 pub const PIC_1_OFFSET: u8 = 32; //CPU has 32 execptions, so the first index that are free is 32 since the exceptions are 0-31
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8; // PIC 1 has 8 interrupts
 
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe {ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) } );
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
 
 //we use lazy static here because the IDT needs to have a static lifetime because it's
 //used the entire time the OS is running. We could use a Box<> but we don't have a heap
@@ -21,15 +32,31 @@ lazy_static!{
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(crate::gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_handler);
         idt
     };
 }
 
-pub static PICS: spin::Mutex<ChainedPics> =
-    spin::Mutex::new(unsafe {ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) } );
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
 
 pub fn init_idt(){
     IDT.load();
+}
+
+extern "x86-interrupt" fn timer_handler(
+    _stack_frame: &mut InterruptStackFrame)
+{
+    print!("x");
+    unsafe {PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8())};
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
