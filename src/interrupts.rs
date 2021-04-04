f6a95c7ebe412b85cbdc35ea08651501b5b26211
Brandon Haley <brandon.haley94@gmail.com>
2021-04-04 16:52:44 -0700

    added pc-keyboard as dependency

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,6 +1,7 @@
 use x86_64::structures::idt::{
     InterruptDescriptorTable,
     InterruptStackFrame,
+    PageFaultErrorCode,
 };
 use crate::{
     println,
@@ -19,6 +20,7 @@ pub static PICS: spin::Mutex<ChainedPics> =
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard,
 }
 
 //we use lazy static here because the IDT needs to have a static lifetime because it's
@@ -32,7 +34,9 @@ lazy_static!{
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(crate::gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_handler);
+        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_handler);
         idt
     };
 }
@@ -57,6 +61,48 @@ extern "x86-interrupt" fn timer_handler(
 {
     print!("x");
     unsafe {PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8())};
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+){
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    crate::hlt_loop();
+}
+
+extern "x86-interrupt" fn keyboard_handler(
+    _stack_frame: &mut InterruptStackFrame)
+{
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static!{
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = 
+            Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore));
+            //handle control lets us map ctrl+[a-z] to unicode characters, not useful now
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe{ port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
+    }
+
+    unsafe { PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8()); }
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
