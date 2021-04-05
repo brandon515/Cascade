ad248114c39dfc8bad64272033554ab12985dce6
Brandon Haley <brandon.haley94@gmail.com>
2021-04-04 18:05:41 -0700

    Added bare bones paging implementation

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,30 @@ use bootloader::{
 };
 use core::panic::PanicInfo;
 use cascade::{
+    memory::active_level_4_table,
     println,
 };
+use x86_64::VirtAddr;
 
 //entry point!
 entry_point!(kernel_main); //this macro checks to make sure the entry point is the correct function signature
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    
     cascade::init();
 
+    let physical_memory_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let level_4_table = unsafe { active_level_4_table(physical_memory_offset) };
+
+    for (i, entry) in level_4_table.iter().enumerate() {
+        if !entry.is_unused(){
+            println!("L4 Entry {}: {:?}", i, entry);
+        }
+    }
 
     #[cfg(test)]
     test_main();
 
     println!("Hello World{}", "!");
-
     cascade::hlt_loop();
 }
 
