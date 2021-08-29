cd173fc798c6b1c6be1e61d905c2c9ee2b60e205
Brandon Haley <brandon.haley94@gmail.com>
2021-08-29 04:12:49 -0700

    added long mode check

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -5,7 +5,10 @@ bits 32 ; 32 bit instructions, this will change to 64 once we go to long mode
 ; 0xb8000 is the VGA buffer for the screen
 start:
   mov esp, stack_top ; ESP is the register with the stack pointer, just set it to the highest memory address because the stack goes down
-  mov dword [0xb8000], 0x2f4b2f4f
+  call check_multiboot
+  call check_cpuid
+  call check_long_mode
+  mov dword [0xb8000], 0x2f4b2f4f ; put OK on the screen
   hlt
 error:
   mov dword [0xb8000], 0x4f524f45 ; ER
@@ -17,6 +20,9 @@ check_multiboot:
   cmp eax, 0x36d76289 ; according to the multiboot spec this must be written to eax when the kernel is loaded
   jne .no_multiboot ; jump to multiboot if the previous comparison isn't equal
   ret
+.no_multiboot:
+  mov al, "0"
+  jmp error
 check_cpuid:
   pushfd ; push the FLAGS into EAX and go up the stack
   pop eax
@@ -34,8 +40,18 @@ check_cpuid:
 .no_cpuid:
   mov al, "1"
   jmp error
-.no_multiboot:
-  mov al, "0"
+check_long_mode: ; check to see if we can enter long mode
+  mov eax, 0x80000000 ; get highest extended function implemented
+  cpuid
+  cmp eax, 0x80000001
+  jb .no_long_mode ; if the return value is less that 0x8000000 then that means there are no extended functions and long mode is impossible
+  mov eax, 0x80000001
+  cpuid
+  test edx, 1 << 29 ; fancy for an AND operation, we're just checking the 29th bit which is 1 if long mode is possible on this cpu
+  jz .no_long_mode
+  ret
+.no_long_mode:
+  mov al, "2"
   jmp error
 
 section .bss
