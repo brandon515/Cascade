1da343a4f538d18b22a3ba6fa21515ad6c1b4b3d
Brandon Haley <brandon.haley94@gmail.com>
2021-11-29 21:20:19 +0000

    added timer interrupt from PIC

--- src/kernel/interrupts.c
+++ src/kernel/interrupts.c
@@ -1,8 +1,13 @@
 #include "interrupts.h"
-#include "printf.h"
 
 __attribute__((interrupt))
 void double_fault_handler(void* interrupt_stack, uint64_t error_code){
   printf("Double Fault");
   __asm__("hlt");
+}
+
+__attribute__((interrupt))
+void timer_handler(void* interrupt_stack){
+  printf("timer");
+  PIC_sendEOI(0);
 }
--- src/kernel/interrupts.h
+++ src/kernel/interrupts.h
@@ -1,6 +1,9 @@
 #ifndef INTERRUPTS_H_
 #define INTERRUPTS_H_
 #include "stdint.h"
+#include "printf.h"
+#include "pic.h"
 
 void double_fault_handler(void* interrupt_stack, uint64_t error_code);
+void timer_handler(void* interrupt_stack);
 #endif
