9be99f2a6af7f9f819228981c9999c5dcb4c01f9
Brandon Haley <brandon.haley94@gmail.com>
2021-11-29 21:19:36 +0000

    offloaded all init functions to their respective files

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -6,6 +6,8 @@
 #include "multiboot2.h"
 #include "../arch/x86_64/start.h"
 #include "memory.h"
+#include <stdbool.h>
+#include "pic.h"
 
 /*struct multiboot_mmap_entry
 {
@@ -23,54 +25,10 @@
 
 void kmain(uint32_t* info, uint64_t* page_table, gdt_entry* gdt){
   cls();
-  create_idt_entry(8, (uint64_t)&double_fault_handler, TRAP_GATE_32); // create double fault handler so we don't go into a triple fault reset cycle
-  //uint64_t *num = (uint64_t*)0xffffffffff;
-  //*num = 50;
-  printf("INITILIZING KERNEL HEAP\n");
-  Sector *head = (Sector*)get_heap_start();
-  head->size = -1;
-  head->next = (head+1);
-  head = head->next;
-  head->size = 0x8000;
-  head->next = NULL;
-  printf("KERNEL HEAP INITILIZED\n");
+  init_PIC();
+  init_idt();
+  init_heap();
   info = (uint32_t*)((uint8_t*)info-2); // for some reason the process from real mode to 64-bit adds 2 bytes to the address
-  uint32_t boot_info_size = info[0]/4; // Grub makes the first 4 bytes of the boot info the size of the structure in bytes
-  for(uint32_t i = 0; i < boot_info_size; i++){
-    if(info[i] == 6){
-      multiboot_tag_mmap *mmap = (multiboot_tag_mmap*)&info[i];
-      if(mmap->entry_size != 24){
-        continue;
-      }
-      printf("Type: %d\n", mmap->type);
-      printf("Size: %d\n", mmap->size);
-      printf("Entry Size: %d\n", mmap->entry_size);
-      printf("Entry Version: %d\n", mmap->entry_version);
-      for(uint32_t j = 0; j < mmap->size; j++){
-        if(mmap->entries[j].type > 5){
-          break;
-        }
-        switch(mmap->entries[j].type){
-          case MULTIBOOT_MEMORY_AVAILABLE:
-            printf("Address: %x Length: %d Type: Available\n", mmap->entries[j].addr, mmap->entries[j].len);
-            break;
-          case MULTIBOOT_MEMORY_RESERVED:
-            printf("Address: %x Length: %d Type: Reserved\n", mmap->entries[j].addr, mmap->entries[j].len);
-            break;
-          case MULTIBOOT_MEMORY_ACPI_RECLAIMABLE:
-            printf("Address: %x Length: %d Type: ACPI Reclaimable\n", mmap->entries[j].addr, mmap->entries[j].len);
-            break;
-          case MULTIBOOT_MEMORY_NVS:
-            printf("Address: %x Length: %d Type: NVS\n", mmap->entries[j].addr, mmap->entries[j].len);
-            break;
-          case MULTIBOOT_MEMORY_BADRAM:
-            printf("Address: %x Length: %d Type: Bad Ram\n", mmap->entries[j].addr, mmap->entries[j].len);
-            break;
-          default:
-            break;
-        }
-      }
-    }
-  }
-  __asm__("cli; hlt");
+  struct multiboot_mmap_entry* a_mem = init_memory(info);
+  while(true){}
 }
